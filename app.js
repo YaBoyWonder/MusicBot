1fb47ee2f02c64e4d2d271b4f670f48a852de139
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-02-21 14:30:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 
 const auth = require('./settings.json');
 
-client.login(auth.token).then(() => console.log('logged')).catch(console.error);
+client.login(settings.token).then(() => console.log('logged')).catch(console.error);
 
 const connections = new Map();
 
