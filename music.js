a76d9570872a26ab99adbd79b4888209c4630bb4
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-04-12 05:45:40 -0400

    Update music.js

--- music.js
+++ music.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 const auth = require('./settings/config.json');
 client.login(settings.token).then(() => console.log('logged')).catch(console.error);
 const connections = new Map();
-
+            
 client.on('message', msg => {
   if (!msg.guild) return;
    if (msg.content.startsWith('m.join')) {
